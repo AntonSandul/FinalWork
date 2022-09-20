5e47c0a47a34757b41941de1610406c1bfa976d7
AntonSandul <anton81818@gmail.com>
2022-09-20 20:55:03 +0700

    added file with code

---
+++ FinalWorkSandul.cs
@@ -0,0 +1,22 @@
+﻿string[] Array =
+{ "Sunday", "2",
+"Monday", "33",
+"Tuersday",
+"Wednesday", "one",
+"Thirsday", ";-)",
+"Friday", "O7",
+"Saturday" };
+Console.WriteLine("[{0}]", string.Join(", ", Array));
+Console.WriteLine();
+string[] ResultArray = new string[Array.Length];
+var realSize = 0;
+foreach (var element in Array)
+{
+    if (element.Length <= 3)
+    {
+        ResultArray[realSize] = element;
+        realSize++;
+    }
+}
+Console.WriteLine("[{0}]", string.Join(" ", ResultArray));
+Console.WriteLine();
